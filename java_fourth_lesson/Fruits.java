b9adc5311036a0a14474277e9b40b5678de50bed
Sushkov Andrey <asushkov2020@gmail.com>
2022-06-13 01:40:53 +0300

    SU

--- java_fourth_lesson/Fruits.java
+++ java_fourth_lesson/Fruits.java
@@ -1,13 +1,22 @@
+/*
+ * Реализовать предметную область: Продажа товаров супермаркета. Создать родительский класс "Супермаркет" (название
+ * отдела, название товара, страна-производитель, розничная цена, поставщик) и дочерние классы:
+ * -"Игрушки" (возрастная группв, тип)
+ * -"Фрукты" (максимальное время хранения, температура хранения)
+ * -"Габаритный товар" (высота, ширина, длина)
+ * Реализовать класс для хранения списка товаров с методом добавления нового товара и методом печати списка товара.
+ * */
+
 package supermarket;
 
 public class Fruits extends Supermarket{
     private int storageTime;
-    private int temprature;
+    private int temperature;
 
     public Fruits(String nameOfDept, String nameOfProduct, String country, int retailPrice, String supplier, int storageTime, int temprature) {
         super(nameOfDept, nameOfProduct, country, retailPrice, supplier);
         this.storageTime = storageTime;
-        this.temprature = temprature;
+        this.temperature = temprature;
     }
 
     public void setStorageTime(int storageTime) {
@@ -19,11 +28,11 @@ public class Fruits extends Supermarket{
     }
 
     public void setTemprature(int temprature) {
-        this.temprature = temprature;
+        this.temperature = temprature;
     }
 
     public int getTemprature() {
-        return temprature;
+        return temperature;
     }
 
     @Override
@@ -36,7 +45,7 @@ public class Fruits extends Supermarket{
                 ", розничная цена=" + retailPrice +
                 ", поставщик='" + supplier + '\'' +
                 ", максимальное время хранения=" + storageTime +
-                ", температура хранения=" + temprature +
+                ", температура хранения=" + temperature +
                 '}';
     }
 }
