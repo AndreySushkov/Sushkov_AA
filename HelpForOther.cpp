1c979dcc7c4ef95f703035a1441dd1ae84f1c85f
Sushkov Andrey <asushkov2020@gmail.com>
2022-06-16 22:00:05 +0300

    Helping

---
+++ HelpForOther.cpp
@@ -0,0 +1,65 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    ifstream fout("out.txt");
+    int counterX = 0,
+        counterY = 0;
+    char ch = ' ';
+    string str;
+
+    //вычисление кол-ва столбцов
+    while (ch != '\n') {
+        fout.get(ch);
+        counterX++;
+    }
+    counterX /= 2;
+    fout.seekg(0);
+
+    //вычисление кол-ва строк
+    while (!fout.eof()) {
+        getline(fout, str);
+        counterY++;
+    }
+    fout.seekg(0);
+
+    //создание двумерного массива
+    int** A = new int*[counterY];
+    for (int i = 0; i < counterY; i++) {
+        A[i] = new int[counterX];
+    }
+
+    //заполнение массива
+    for (int i = 0; i < counterY; i++) {
+        for (int j = 0; j < counterX; j++) {
+            fout >> A[i][j];
+        }
+    }
+
+    //вывод массива
+    for (int i = 0; i < counterY; i++) {
+        for (int j = 0; j < counterX; j++) {
+            cout << A[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    int product = 1;
+    //основной алгоритм
+    for (int i = 0; i < counterY; i++) {
+        for (int j = 0; j < counterX; j++) {
+            if (A[i][j] % 2 == 0) {
+                product *= A[i][j];
+            }
+        }
+    }
+
+    cout << product << endl;
+
+    fout.close();
+}
+
---
+++ HelpForOther2.cpp
@@ -0,0 +1,53 @@
+﻿#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int main()
+{
+    ifstream fout("out.txt");
+    int num;
+
+    //создание массива A
+    int* A = new int[10];
+
+    //заполнение массива A
+    for (int i = 0; i < 10; i++) {
+        fout >> A[i];
+    }
+
+    //вывод массива A
+    for (int i = 0; i < 10; i++) {
+        cout << A[i] << " ";
+    }
+    cout << endl;
+
+    cin >> num;
+
+    //вычисление размера массива B
+    int counter = 0;
+    for (int i = 0; i < 10; i++) {
+        if (A[i] == num) {
+            counter++;
+        }
+    }
+
+    //создание массива B
+    int* B = new int[counter];
+
+    int n = 0;
+    //заполнение массива B
+    for (int i = 0; i < 10; i++) {
+        if (A[i] == num) {
+            B[n] = i;
+            n++;
+        }
+    }
+
+    for (int i = 0; i < counter; i++) {
+        cout << B[i] << " ";
+    }
+    cout << endl;
+
+    fout.close();
+}
